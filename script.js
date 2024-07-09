0c9e1b87b76924a8cb06acdc98663e792a9afc26
Nainesh Raval <145770394+Naineshrawal@users.noreply.github.com>
2024-07-09 12:26:25 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,74 +1,30 @@
-let input = document.getElementById("input")
-let error = document.getElementById("error")
-let guesses = document.getElementById("guesses")
-let submitbtn = document.getElementById("submitbtn")
-let resetBtn = document.getElementById("resetBtn")
-let atempt = document.getElementById("atempt")
+const generateButton = document.querySelector("#memeButton")
+const memeTitle = document.getElementById("memeTitle")
+const memeAuther = document.querySelector(".memeAuther")
+const memeImg = document.querySelector(".memeImg")
 
-let atempted = 0
-let randomNumber;
-let guessArr = []
+function memeGenerator(){
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then(res => res.json())
+    .then(data => {
 
-
-let generateNo = ()=>{
-    randomNumber =  Math.floor(Math.random() * 100)
-}
-
-function reset(){
-    atempt.innerHTML = ""
-    guesses.innerHTML = ""
-    generateNo()
-    input.value = ""
-    error.style.color = "red"
-    error.innerHTML = ""
-    resetBtn.style.display = "none"
-    submitbtn.style.display = "block"
-    guessArr = []
-}
-reset()
+        const {title, author, url} = data
+        console.log(title, author, url);
+        memeTitle.innerHTML = title
+        memeAuther.innerHTML = author
+        memeImg.src = url
 
 
-const hanbleSubmit = ()=>{
-    
-  
-   
-    if(input.value.length > 0){
-        error.innerHTML = ""
-        if(input.value == randomNumber ){
-            
-            error.innerHTML = `Winner - Winner <br/>Congratulations you Guess Right <br> Ther Right Answer is </br> ${randomNumber}`
-            error.style.color = "green"
-            submitbtn.style.display = "none"
-            resetBtn.style.display = "block"
+        // memeTitle.innerHTML = data.title
+        // memeAuther.innerHTML = `Meme By : ${data.author}`  
+        // memeImg.src = data.url
 
-            
-            }else{
-                    if (input.value > 100) {
-                        error.innerHTML = "Number Shouldn't be Greater then 100"
-                    }else{
-                        
-                        if(input.value>randomNumber){
-
-                            error.innerHTML = "Your Guess No is High"
-                        }else if(input.value<randomNumber){
-                            error.innerHTML = " Your Guess No is low"
-
-                    }
-
-                    guessArr.push(input.value)
-                    guesses.innerHTML = "Your Guesses : " + guessArr.join(", ")
-                    // guesses.innerHTML += input.value + ", "
-                    
-                    
-                    
-                }
-            }
-            
-            atempted++;
-            atempt.innerHTML = atempted
-        }else{
-            error.innerHTML = "Please guess the number"
-        }
+ }
         
-        input.value = ""
+    )
+    
 }
+
+generateButton.addEventListener("click", memeGenerator)
+
+memeGenerator()
